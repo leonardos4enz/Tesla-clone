167bb2bae32e8a6be681b95843ea4df4e501b781
leonardos4enz <leonardorsaenz@gmail.com>
2023-02-26 13:43:13 -0600

    Bandeja de entrada - más datos

--- lib/data/inbox_data.dart
+++ lib/data/inbox_data.dart
@@ -6,4 +6,11 @@ List<Map<String, dynamic>> inboxData = [
     'description':
         'Ahora está utilizando la versión actualizada de la bandeja de entrada. Compruebe su bandeja de entrada para obtener información importante sobre sus productos y servicios Tesla.'
   },
+  {
+    'id': 1,
+    'date': DateTime(2023, 2, 26, 8, 15, 00),
+    'title': 'Tesla llega a Nuevo León',
+    'description':
+        'Queremos anunciar que nuestra llegada ahora es un hecho, instalaremos nuestra nueva planta en Santa Catarina, Nuevo León, México. Es un lugar lleno de oportunidades que no se debe desaprovechar. '
+  },
 ];
